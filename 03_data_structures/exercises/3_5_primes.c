981021661ff4668cad8461ef30c263188775dda9
Malek N <Malek@Necibi.us>
2023-07-20 19:11:00 -0500

    primes sieve, removed compiled binaries

---
+++ 03_data_structures/exercises/3_5_primes.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define BUFFER_SIZE 31
+int main(int argc, char* argv[]) {
+    
+    printf("Prime tester, how many values should we test? ");
+
+    char* buffer;
+    char input_buffer[BUFFER_SIZE + 1] = {0};
+    
+    if (argc > 1) {
+        buffer = argv[1];
+    } else {
+        if ( !fgets(input_buffer, BUFFER_SIZE, stdin) )
+            return -1;
+        
+        buffer = (char*)input_buffer;
+    }
+        
+    char* end;
+    long max_val = strtol(buffer, &end, 10);
+    if (buffer == end)
+        return -1;
+    
+    // 0 : non-prime
+    // 1 : prime
+    bool* primes_array = calloc(1 + max_val, sizeof(bool));
+    if (!primes_array)
+        return -2;
+    
+    for (long i = 2; i <= max_val; i++) {
+        for (long j = i*2; j <= max_val; j += i) {
+            primes_array[j] = true;
+        }
+    }
+    
+    printf("\nPrimes:\n");
+    long prime_count = 0;
+    for (long i = 1; i <= max_val; i++) {
+        if (!primes_array[i]) {
+            //printf("%ld, ", i);
+            prime_count++;
+        }
+    }
+    printf("\n\n%ld primes of\n%ld total\n", prime_count, max_val);
+    
+    
+    free(primes_array);
+}
