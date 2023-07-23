04a861b6559c1e1beea6b5551f2c0953c9e7ba25
Malek N <Malek@Necibi.us>
2023-07-23 15:50:46 -0500

    ADTs : queue, stack similar format : ch4

---
+++ 04_abstract_data_types/queue.c
@@ -0,0 +1,160 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<stdbool.h>
+
+#define DTYPE int
+#define ERROR_VAL INT_MIN
+
+// enqueue to tail
+// dequeue from head
+
+// Queue    : pointer to a QueueMeta
+// QueueMeta: container with pointers to head and tail QueueNode's
+// QueueNode: container that actually stores one value to store
+
+typedef struct ListNode QueueNode;
+typedef struct QueueMeta* Queue;
+
+struct QueueMeta {
+    QueueNode* head;
+    QueueNode* tail;
+};
+
+struct ListNode {
+    DTYPE value;
+    struct ListNode* next;
+};
+
+// private
+QueueNode* initQueueNode(DTYPE value) {
+    QueueNode* node = malloc( sizeof(QueueNode) );
+    if (!node) return NULL;
+
+    node->value = value;
+    node->next = NULL;
+    return node;
+}
+
+Queue initQueue() {
+    Queue queue = calloc(1, sizeof(struct QueueMeta));
+    queue->head = NULL;
+    queue->tail = NULL;
+
+    return queue;
+}
+
+bool isEmpty(Queue queue) {
+    if (!queue) {
+        printf("WARNING: isEmpty called with deleted/NULL Queue\n");
+        return true;
+    }
+    if (!queue->head || !queue->tail) {
+        // should both be NULL simultaneously
+        if (queue->head || queue->tail) {
+            printf("ERROR isEmpty() : only 1 of head/tail is NULL ? %p , %p\n", queue->head, queue->tail);
+        }
+    }
+    return (!queue->head || !queue->tail);
+}
+
+// return ERROR_VAL if empty/deleted queue provided
+DTYPE dequeue(Queue queue) {
+    if (!queue || isEmpty(queue)) {
+        return ERROR_VAL;
+    }
+    
+    QueueNode *old_head = queue->head;
+    DTYPE value = old_head->value;
+    
+    QueueNode *new_head = old_head->next;
+    queue->head = new_head;
+
+    if (NULL == new_head) {
+        // was only 1 item in queue, now 0
+        queue->tail = NULL;
+    }
+    
+    free(old_head);
+    return value;
+}
+
+bool enqueue(Queue queue, DTYPE value) {
+    if (!queue) {
+        return false;
+    }
+    
+    QueueNode* node = initQueueNode(value);
+    if (!node) {
+        return false;
+    }
+    node->next = NULL;  // redundant but safe
+
+    if ( isEmpty(queue) ) {
+        // only 1 item now, head == tail
+        queue->head = node;
+    
+    } else {
+        // extend the tail of linked list with new node
+        queue->tail->next = node;
+    }
+    // update QueueMeta with new tail
+    queue->tail = node;
+
+    return true;
+}
+
+void deleteQueue(Queue* _queue) {
+    if (!_queue || !*_queue) {
+        return;
+    }
+
+    Queue queue = *_queue;
+    // remove all items from Queue
+    while ( !isEmpty(queue) ) {
+        dequeue(queue);
+    }
+    
+    // delete the queue's metadata
+    free(queue);
+    
+    *_queue = NULL;
+}
+
+int main(void) {
+    Queue q = initQueue();
+    enqueue(q, 12);
+    
+    printf("%d\n", dequeue(q));
+    
+    enqueue(q, 17);
+    printf("%d\n", dequeue(q));
+
+    enqueue(q, 19);
+    enqueue(q, 19282);
+    printf("%d\n", dequeue(q));
+    enqueue(q, -2189);
+
+    printf("%d\n", dequeue(q));
+    printf("%d\n", dequeue(q));
+    
+    // dequeue more than enqueue
+    printf("%d (should be %d)\n", dequeue(q), ERROR_VAL);
+    
+    enqueue(q, 38);
+    enqueue(q, 239792);
+    printf("%d\n", dequeue(q));
+
+    deleteQueue(&q);
+
+
+    // should fail with ERROR_VAL
+    enqueue(q, 18);
+    enqueue(q, 1);
+    enqueue(q, -9304792);
+    printf("Dequeue after deletion returns ERROR_VAL? %s\n", (dequeue(q) == ERROR_VAL) ? "true" : "false");
+
+    deleteQueue(&q);
+
+    return 0;
+}
--- 04_abstract_data_types/stack.c
+++ 04_abstract_data_types/stack.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<limits.h>
+#include<stdbool.h>
 
 #define DTYPE int
 #define ERROR_VAL INT_MIN
@@ -8,81 +9,178 @@
 // push to head
 // pop from head
 
+// Stack    : pointer to a StackMeta
+// StackMeta: container with pointers to head and tail StackNode's
+// StackNode: container that actually stores one value to store
+
 typedef struct ListNode StackNode;
-typedef struct ListNode* Stack;
+typedef struct StackMeta* Stack;
+
+struct StackMeta {
+    StackNode* head;
+
+};
+
 struct ListNode {
     DTYPE value;
     struct ListNode* next;
 };
 
-Stack initStack(DTYPE value) {
-    StackNode* node = calloc(1, sizeof(StackNode));
+// private
+StackNode* initStackNode(DTYPE value) {
+    StackNode* node = malloc( sizeof(StackNode) );
+    if (!node) return NULL;
+
     node->value = value;
     node->next = NULL;
     return node;
 }
 
-// return ERROR_VAL if empty stack provided
-DTYPE pop(Stack *stack) {
-    if (!stack || !*stack)
+Stack initStack() {
+    Stack stack = calloc(1, sizeof(struct StackMeta));
+    stack->head = NULL;
+
+    return stack;
+}
+
+
+bool isEmpty(Stack stack) {
+    if (!stack) {
+        printf("WARNING: isEmpty called with deleted/NULL Stack\n");
+        return true;
+    }
+    return (!stack->head);
+}
+
+
+
+
+
+
+
+// return ERROR_VAL if empty/deleted stack provided
+DTYPE pop(Stack stack) {
+    if (!stack || isEmpty(stack)) {
         return ERROR_VAL;
+    }
     
-    StackNode *old_head = *stack;
+    StackNode *old_head = stack->head;
     DTYPE value = old_head->value;
     
     StackNode *new_head = old_head->next;
-    *stack = new_head;
+    stack->head = new_head;
+    
     free(old_head);
-
     return value;
 }
 
-void push(Stack *stack, DTYPE value) {
-    if (NULL == stack)
-        return;
-    if (NULL == *stack) {
-        *stack = initStack(value);
-        return;
-    }
 
-    StackNode* node = initStack(value);
-    node->next = *stack;
-    *stack = node;
+
+
+
+
+
+bool push(Stack stack, DTYPE value) {
+    if (!stack) {
+        return false;
+    }
+    
+    StackNode* node = initStackNode(value);
+    if (!node) {
+        return false;
+    }
+    node->next = stack->head;
+
+    stack->head = node;
+    return true;
 }
 
-void deleteStack(Stack* stack) {
-    StackNode* head = *stack;
-    while (head) {
-        StackNode* node = head;
-        head = node->next;
 
-        free(node);
+
+
+
+
+
+
+
+
+void deleteStack(Stack* _stack) {
+    if (!_stack || !*_stack) {
+        return;
     }
-    *stack = NULL;
+    
+    Stack stack = *_stack;
+    // remove all items from stack
+    while ( !isEmpty(stack) ) {
+        pop(stack);
+    }
+    
+    free(stack);
+    *_stack = NULL;
 }
+
+
 
 int main(void) {
+    Stack s = initStack();
+    push(s, 12);
+    printf("%d\n", pop(s));
     
-    Stack s = initStack(12);
-    printf("%d\n", pop(&s));
+    push(s, 17);
+    printf("%d\n", pop(s));
     
-    push(&s, 17);
-    printf("%d\n", pop(&s));
+    push(s, 19);
+    push(s, 19282);
+    push(s, -2189);
     
-    push(&s, 19);
-    push(&s, 19282);
-    push(&s, -2189);
-    
-    pop(&s);
+    pop(s);
 
     deleteStack(&s);
     
-    // should still work, and safely
-    push(&s, 18);
-    push(&s, 1);
-    push(&s, -9304792);
-    pop(&s);
+    
+
+    // must all fail with ERROR_VAL, and without memory leaks
+    push(s, 18);
+    push(s, 1);
+    push(s, -9304792);
+    pop(s);
 
     deleteStack(&s);
+
+    
+    Stack s2 = initStack();
+    push(s2, 12);
+    
+    printf("%d\n", pop(s2));
+    
+    push(s2, 17);
+    printf("%d\n", pop(s2));
+
+    push(s2, 19);
+    push(s2, 19282);
+    printf("%d\n", pop(s2));
+    push(s2, -2189);
+
+    printf("%d\n", pop(s2));
+    printf("%d\n", pop(s2));
+    
+    // pop more than push
+    printf("%d (should be %d)\n", pop(s2), ERROR_VAL);
+    
+    push(s2, 38);
+    push(s2, 239792);
+    printf("%d\n", pop(s2));
+
+    deleteStack(&s2);
+
+
+    // should fail with ERROR_VAL
+    push(s2, 18);
+    push(s2, 1);
+    push(s2, -9304792);
+    printf("pop after deletion returns ERROR_VAL? %s\n", (pop(s2) == ERROR_VAL) ? "true" : "false");
+
+    deleteStack(&s2);
+
     return 0;
 }
