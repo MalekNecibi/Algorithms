896cb28b4825238a8a5369caa20946dc541b2695
Malek N <Malek@Necibi.us>
2023-07-25 13:56:12 -0500

    recursion hanoi : ch05

--- 05_recursion_trees/hanoi_towers.c
+++ 05_recursion_trees/hanoi_towers.c
@@ -1,69 +1,75 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include<limits.h>
+
 
 #define STACK_DTYPE int
-#include "../04_abstract_data_types/stack.h"
+#define STACK_ERROR_VAL INT_MIN
+#include "../utils/stack.h"
 
 #ifndef NUM_DISKS
-#define NUM_DISKS 2
+#define NUM_DISKS 5
 #endif
 
+enum Tower { Left = 0 , Middle = 1 , Right = 2 };
+enum Direction { Leftward = (-1) , Rightward = (+1) };
+
+size_t hanoi(int num_disks);
+
 int main(void) {
-    
-    //3 stacks
-    Stack l = initStack();
-    Stack m = initStack();
-    Stack r = initStack();
-    
-    Stack* towers[3] = { l , m , r }
-
-    (void)l;
-    (void)m;
-    (void)r;
-
-    for (size_t i = NUM_DISKS; i > 0; i--) {
-        push(m, i);
-    }
-    
-    hanoi(towers, m
+    size_t num_moves = hanoi(NUM_DISKS);
+    printf("\n%zu moves\n", num_moves);
     
     return 0;
 }
 
-// implement peek()
 
-hanoi(Stack towers[3], int tower, int disk, bool right) {
-    // find which stack it's in
+size_t _hanoi(Stack towers[3], int tower, int depth, enum Direction dir) {
+    int orig_tower = tower % 3;
+    int dir_tower  = (tower + dir + 3) % 3;
+    int ndir_tower  = (tower - dir + 3) % 3;
+    
+    size_t num_moves = 0;
 
-    if (1 == disk) {
-        int disk = pop(towers[tower]);  // disk 1
-        int new_tower = (right) ? (tower+1) % 3 : (tower-1+3) % 3;
-        push(towers[new_tower], disk);
-
+    if (1 == depth) {
+        int disk = pop(towers[orig_tower]);
+        printf("disk %d: %d -> %d\n", disk, tower, dir_tower);
+        push(towers[dir_tower], disk);
+        return 1;
+        
+    } else if (depth > 1) {
+        num_moves += _hanoi(towers, orig_tower, depth-1, -dir);
+        num_moves += _hanoi(towers, orig_tower, 1      , dir);    
+        num_moves += _hanoi(towers, ndir_tower, depth-1, -dir);    
     } else {
-        // if the current value is smaller than me, have to move it first
-        if (
-        hanoi(towers, tower, 
+        printf("error: invalid depth? %d\n", depth);
     }
+    return num_moves;
 }
 
-// hanoi
-// moving 5
-    // move 4 left
-    // move 5 right
-    // move 4 left
+size_t hanoi(int num_disks) {
+    // 1 stack per tower
+    Stack l = initStack();
+    Stack m = initStack();
+    Stack r = initStack();
+    
+    Stack towers[3] = { l , m , r };
+    
+    // pre-populate center-stack with disks
+    for (size_t i = num_disks; i > 0; i--) {
+        push(m, i);
+    }
+    
+    enum Tower tower = Middle;
+    size_t num_moves = _hanoi(towers, tower, num_disks, Rightward);
 
-// moving 4
-    // move 3 left
-    // move 4 right
-    // move 4 left
+    deleteStack(&l);
+    deleteStack(&m);
+    deleteStack(&r);
+    
+    return num_moves;
+}
 
-// moving N
-    // move N-1 left
-    // move N right
-    // move N-1 left
 
-// moving 1
-    // move 1 right
 
